26bc60464a521e065d9b3b92274213b3939ec6bc
dimaSerebro <dima.agstivjobs@gmail.com>
2021-10-31 18:48:26 +0200

    data types lessons 6

---
+++ js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+let number = 3.4; //number
+
+console.log(-5/0); // infinity
+
+console.log('string' * 5); // NaN
+
+const person = `5`; //string
+
+const bool = false; //boolean (true or false)
+
+//console.log(something); //null
+
+let hot;
+console.log(hot); //undefined
+// Объект
+const obj = {
+   name: "Dima",
+   age: 24,
+   isMarried: false
+};
+
+console.log(obj.name);
+//console.log(obj["name"]);
+//массив это частный случай объекта
+   //      0            1            2   3           4   5
+let arr = ['plump.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[2]);
+
+
+
+
+
+
