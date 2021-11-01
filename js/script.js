08f5f2f1f02c0ff3c8216732312a8310658ad178
dimaSerebro <dima.agstivjobs@gmail.com>
2021-11-01 19:44:22 +0200

    js operators lessons 9

--- js/script.js
+++ js/script.js
@@ -1,11 +1,42 @@
 "use strict";
 
-const category = "toys";
+/* console.log('arr' + " - object ");
+console.log( 4 + " - object ");
+console.log( 4 + +"4");  */
 
-//console.log('https:someurl.com/' + category + '/' +'4');
+let incr = 10,
+    decr = 10;
 
-console.log(`https:someurl.com/${category}/5`);
+/* incr++;//увеличивает на 1(постфиксная форма записи)
+--decr;//уменьшает на 1 (префиксная форма записи) */
 
-const user = "Dima";
-alert(`Hi ${user}`)
-console.log(`Привет ${user}`);
+//возвращает старое значение до увеличения или уменьшения
+console.log(incr++);
+console.log(decr--);
+//возвращает увеличенное или уменьшенное на 1 значение
+console.log(++incr);
+console.log(--decr);
+
+console.log(10%4);// остаток от деления
+
+console.log(2*4 == 8);// true
+console.log(2*4 == '8'); // сравнивает значения и не смотрит на их тип
+console.log(2*4 === '8');// false строгое сравнение(идет сравнение числа и строки)
+
+// Оператор и && / или ||
+
+/* const isOpen = true, // && если один из подопечных будет false они оба будут ложны
+      isClosed = false;
+console.log(isOpen && isClosed);// false */
+/*
+const isOpen = false, // || или  , если одно из условий будет true ответ будет true
+      isClosed = false;
+console.log(isOpen || isClosed);// false */
+  // оператор отрицания "!"
+const isOpen = false,
+      isClosed = false;
+console.log(isOpen || !isClosed);// true
+
+console.log(2 + 2 * 2 == 8);// false будь внимателен с приорететами операторов
+console.log(2 + 2 * 2 != 6);// false
+console.log(2 + 2 * 2 !== '6');// true
