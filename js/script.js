b6ec597838906b4a42f6a3c9303825c384453516
dimaSerebro <dima.agstivjobs@gmail.com>
2021-11-09 21:15:13 +0200

    arrays and pseudo-classes (lessons_21)

--- js/script.js
+++ js/script.js
@@ -1,52 +1,39 @@
 "use strict";
 
-const options = {
-   name: 'test',
-   height: 1024,
-   width: 1024,
-   colors: {
-      border: 'red',
-      bg: 'black'
-   }
-};
-//console.log(options['colors']['border']);
-// delete options.name;
-// console.log(options);
+const arr = [10, 23, 25, 17, 0, 43, 38, 84];
+arr.sort();
+console.log(arr);
 
-console.log(Object.keys(options).length);
-// Методы это действия которые может совершать наш объект!
-const quality = {
-   kindness: true,
-   honesty: true,
-   greed: false,
-   poverty: false,
-   loyalty: {
-      treason: false,
-      notTreason: true
-   },
-   makeTest: function() {
-      console.log('Test');
-   }
-};
-
-quality.makeTest();
-
-const {treason, notTreason} = quality.loyalty;// Деструктиризация это когда мы можем достать коробку из коробки и напрямую обратиться к ней 
-console.log(treason);
-
-console.log(Object.keys(quality).length);
-
-/* let counter = 0;
-for (let key in options) {
-   if (typeof (options[key]) === 'object') {
-      for (let i in options[key]) {
-         console.log(`Свойство ${i} имеет значение ${options[key] [i]}`);
-      }
-   } else {
-      console.log(`Свойство ${key} имеет значение ${options[key]}`);
-      counter++;
-   }
+function compareNum (a, b) {
+   return a - b;
 }
-console.log(counter); */
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+/* arr.forEach (function (item, i, arr) {
+   console.log(`${i}: ${item} внутри массива ${arr}`);
+}); */
+
+//arr.pop();
+// arr.push(11);
+
+// console.log(arr);
+
+// Перебор массива
+/* for (let i = 0; i < arr.length; i++) {
+   console.log(arr[i]);
+} */
+
+// Перебор массива
+/* for (let value of arr) {
+   console.log(value);
+} */
+
+
+/* const str = prompt("", "");
+const products = str.split(", ");
+products.sort();
+console.log(products.join("; ")); */
 
 
