008dce0be35c451d72f0945b8a549d0fee689475
dimaSerebro <dima.agstivjobs@gmail.com>
2021-11-10 13:31:10 +0200

    OOP basics prototypal - oriented inheritance (lessons_23)

--- js/script.js
+++ js/script.js
@@ -1,96 +1,28 @@
 "use strict";
-//Передача по ссылке или передача по значению spred оператор
+// OOP basics prototypal - oriented inheritance
+let str = 'string';
+let strObj = new String(str);
+console.log(typeof(str));
+console.log(typeof(strObj));
 
-// let a = 5,
-//     b = a;
-
-// b = b + 5;
-// console.log(b);
-// console.log(a);
+console.dir([1, 3, 5, 7]);
 
 
-// const obj = {
-//   a: 5,
-//   b: 1
+const solder = {
+	heals: 400,
+	armor: 100,
+	sayHello: function () {
+		console.log('hello');
+	}
+};
+const john = Object.create(solder);
+
+// const john = {
+// 	heals: 100
 // };
 
-// const copy = obj;//  На этом этапе мы не создаем новый объект сданнми а создаем ссылку на obj изменя copy мы будем изменять наш obj
-// copy.a = 10;
-// console.log(copy);
-// console.log(obj);
-
-function copy (mainObj) {
-  let objCopy = {};
-
-  let key;
-	for (key in mainObj) {
-		objCopy[key] = mainObj[key];
-	}
-
-	return objCopy;
-}
-
-
-const numbers = {
-	a: 4,
-	b: 7,
-	c: {
-		x: 5,
-		y: 9
-	}
-};
-
-const newNumbers = copy(numbers);
-
-numbers.a = 10;
-numbers.c.x = 15;
-
-// console.log(newNumbers);
-// console.log(numbers);
-
-const add = {
-	d: 4,
-	e: 1
-};
-
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-console.log(add);
-console.log(clone);
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-const newAarray = [...oldArray];// работа spread оператора
-
-newArray[1] = 'acsdfc';
-console.log(newArray);
-console.log(oldArray);
-console.log(newAarray);
-
-const video = ['youtube', 'gidonline', 'rutube'],
-			blogs = ['wordpress', 'livejournal', 'bloggers'],
-			internet = [...video, ...blogs, 'vk', 'facebook'];
-
-console.log(internet);
-
-function log (a, b, c) {
-	console.log(a);
-	console.log(b);
-	console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-const q = {
-	one: 1,
-	two: 2
-};
-
-const newObj = {...q};
-newObj.one = 4;
-console.log(newObj);
-console.log(q);
+// john.__proto__ = solder;
+// Object.setPrototypeOf(john, solder);
+//console.log(john);
+//console.log(john.armor);
+john.sayHello();
