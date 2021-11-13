4f98da5c9c3765c41ae9c8d235845f43af12f62c
dimaSerebro <dima.agstivjobs@gmail.com>
2021-11-13 23:13:24 +0200

    Actionss with elements on the pas(lessons_29)

--- js/script.js
+++ js/script.js
@@ -1,26 +1,60 @@
 'use strict';
-//!                    Получение элементов со страницы
+//!               Действие с элементами на странице
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      wrapper = document.querySelector('.wrapper'),
+      hearts = wrapper.querySelectorAll('.heart'),
+      oneHeart = wrapper.querySelector('.heart');
 
-const box = document.getElementById('box');
+box.style.backgroundColor = 'blue';
+box.style.width = '500px';
 
-console.log (box);
+box.style.cssText = 'background-color: yellow; width: 850px';
 
-const btns = document.getElementsByTagName('button');//*  мы получаем колекцию из кнопок он же массив , что бы получить одну кнопку в конец прописываем в квадратных скобках нужный элемент по порядку [1]
+btns[1].style.borderRadius = '100%';
+btns[1].style.width = '50px';
+btns[1].style.height = '50px';
+circles[0].style.backgroundColor = 'green';
 
-console.log(btns[1]);//* или выбираем нужный элемент когда будем делать изменения над ними
-
-const circle = document.getElementsByClassName('circle');
-//!  если мы работаем с классом то мы не ставим точку пред обьявлением елемента
-
-console.log(circle);
-
-const hearts = document.querySelectorAll('.heart');
-//!  если мы работаем с селектором мы ставим точку перед елементом поиска
-
+// for (let i = 0; i < hearts.length; i++) {
+//    hearts[i].style.backgroundColor = 'violet';
+// }
 
 hearts.forEach( item => {
-   console.log(item);
+   item.style.backgroundColor = 'violet';
 });
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+const div = document.createElement('div');
+// const text = document.createTextNode('I was here');
+
+div.classList.add('black');
+div.style.width = '250px';
+div.style.color = 'green';
+
+
+
+wrapper.append(div); //* вставляет в конец родителя  элемент
+// wrapper.appendChild(div); //* раньше
+
+//wrapper.prepend(div);//* вставляет в начало родителя элемент
+
+// hearts[0].before(div);
+// hearts[0].after(div);
+// wrapper.insertBefore(div, hearts[1]); //* раньше
+
+// circles[0].remove(); //* удаление элемента
+// wrapper.removeChild(hearts[1]); //* раньше
+
+hearts[0].replaceWith(circles[0]);
+// wrapper.replaceChild(circles[0], hearts[0]);//* раньше
+
+div.innerHTML = "<h1>Hello world</h1>";// выведет заголовок первого порядка
+
+// div.textContent = '<h1>Hello world</h1>'; // выведет все что мы написали
+
+div.insertAdjacentHTML("afterend", '<h2>Hello world</h2>');
+
+
+
+
