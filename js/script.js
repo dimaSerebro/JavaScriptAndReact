92c64d299a3545815b16daeaba8294715cdcfea8
dimaSerebro <dima.agstivjobs@gmail.com>
2021-11-15 12:46:31 +0200

    Навигация по DOM - элементам, data-атрибуты, преимущество for_of (lessons_32)

--- js/script.js
+++ js/script.js
@@ -1,36 +1,20 @@
 "use strict";
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
 
-const btns = document.querySelectorAll('button'),
-      overlay = document.querySelector('.overlay');
+// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentElement);
 
-// btn.onclick = function() {
-//     alert('Click');
-// };
-// btn.onclick = function() {
-//     alert('Second click');
-// };
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 
-// let i = 0;
-const deleteElement = (e) => {
-    console.log(e.currentTarget);
-    console.log(e.type);
-    // i++;
-    // if (i == 1) {
-    //     btn.removeEventListener('click', deleteElement);
-    // }
-};
+for (let node of document.body.childNodes) {
+    if(node.nodeName == '#text') {
+        continue;
+    }
 
-//  btn.addEventListener('click', deleteElement);
-//  overlay.addEventListener('click', deleteElement);
-
-btns.forEach(btn => {
-  btn.addEventListener('click', deleteElement, {once: true});
-});
-
- const link = document.querySelector('a');
-
- link.addEventListener('click', function(event) {
-   event.preventDefault();
-
-   console.log(event.target);
- });
+    console.log(node);
+}
