e7714a5c04af8d1ab1530aab3affd85ee21991be
dimaSerebro <dima.agstivjobs@gmail.com>
2021-11-15 12:06:57 +0200

    События и их обработчики (lessons_31)

--- js/script.js
+++ js/script.js
@@ -1,60 +1,36 @@
-'use strict';
-//!               Действие с элементами на странице
-const box = document.getElementById('box'),
-      btns = document.getElementsByTagName('button'),
-      circles = document.getElementsByClassName('circle'),
-      wrapper = document.querySelector('.wrapper'),
-      hearts = wrapper.querySelectorAll('.heart'),
-      oneHeart = wrapper.querySelector('.heart');
+"use strict";
 
-box.style.backgroundColor = 'blue';
-box.style.width = '500px';
+const btns = document.querySelectorAll('button'),
+      overlay = document.querySelector('.overlay');
 
-box.style.cssText = 'background-color: yellow; width: 850px';
+// btn.onclick = function() {
+//     alert('Click');
+// };
+// btn.onclick = function() {
+//     alert('Second click');
+// };
 
-btns[1].style.borderRadius = '100%';
-btns[1].style.width = '50px';
-btns[1].style.height = '50px';
-circles[0].style.backgroundColor = 'green';
+// let i = 0;
+const deleteElement = (e) => {
+    console.log(e.currentTarget);
+    console.log(e.type);
+    // i++;
+    // if (i == 1) {
+    //     btn.removeEventListener('click', deleteElement);
+    // }
+};
 
-// for (let i = 0; i < hearts.length; i++) {
-//    hearts[i].style.backgroundColor = 'violet';
-// }
+//  btn.addEventListener('click', deleteElement);
+//  overlay.addEventListener('click', deleteElement);
 
-hearts.forEach( item => {
-   item.style.backgroundColor = 'violet';
+btns.forEach(btn => {
+  btn.addEventListener('click', deleteElement, {once: true});
 });
 
-const div = document.createElement('div');
-// const text = document.createTextNode('I was here');
+ const link = document.querySelector('a');
 
-div.classList.add('black');
-div.style.width = '250px';
-div.style.color = 'green';
+ link.addEventListener('click', function(event) {
+   event.preventDefault();
 
-
-
-wrapper.append(div); //* вставляет в конец родителя  элемент
-// wrapper.appendChild(div); //* раньше
-
-//wrapper.prepend(div);//* вставляет в начало родителя элемент
-
-// hearts[0].before(div);
-// hearts[0].after(div);
-// wrapper.insertBefore(div, hearts[1]); //* раньше
-
-// circles[0].remove(); //* удаление элемента
-// wrapper.removeChild(hearts[1]); //* раньше
-
-hearts[0].replaceWith(circles[0]);
-// wrapper.replaceChild(circles[0], hearts[0]);//* раньше
-
-div.innerHTML = "<h1>Hello world</h1>";// выведет заголовок первого порядка
-
-// div.textContent = '<h1>Hello world</h1>'; // выведет все что мы написали
-
-div.insertAdjacentHTML("afterend", '<h2>Hello world</h2>');
-
-
-
-
+   console.log(event.target);
+ });
