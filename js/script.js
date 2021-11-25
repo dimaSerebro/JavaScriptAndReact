d6cb3193b05b6df769c79bdca3a3b41fece8dc43
dimaSerebro <dima.agstivjobs@gmail.com>
2021-11-25 15:34:31 +0200

    Class list and event delegation (lessons_37)

--- js/script.js
+++ js/script.js
@@ -1,19 +1,54 @@
 "use strict";
 
-const p = document.querySelectorAll('p');
-console.log(p);
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
+
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(0));
+// console.log(btns[0].classList.add('red', 'blabla'));
+console.log(btns[1].classList.add('red'));
+// console.log(btns[2].classList.add('green'));
+// console.log(btns[4].classList.add('blue'));
+// console.log(btns[6].classList.add('violet'));
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
+
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// }
+
+btns[0].addEventListener('click', () => {
+    // if (!btns[1].classList.contains('red')) {
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red');
+    // }
+
+    btns[1].classList.toggle('red');
+});
+
+//* Делегирование
+// console.log(btns[0].className); //! className лучше не использовать
+
+wrapper.addEventListener('click', (event) => {
+   // if (event.target && event.target.tagName == 'BUTTON') {
+   //    console.log('Hello');
+   // }
+   // любят использовать разраб гугл
+   if (event.target && event.target.matches("button.red")) {
+      console.log('Hello');
+   }
+
+});
 
 
+//? если будем использовать метот перебора то на новую добавленную нами кнопку делегирование не бдет расспостраняться
+// btns.forEach( btn => {
+//    btn.addEventListener('click', () => {
+//       console.log('Hello');
+//    });
+// });
 
-
-function loadScrip(src) {
-
-    const script = document.createElement('script');
-    script.src = src;
-    script.async = false; 
-    document.body.append(script);
-
-}
-
-loadScrip("js/test.js");
-loadScrip("js/some.js");
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
